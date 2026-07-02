c81fa7b57f48c32409c4d5f0d038e8a174d9fcdb
nothing2454198-web <admin@studentmanagement.local>
2026-07-02 14:12:27 +0530

    Fix: Allow all Render domains in ALLOWED_HOSTS

--- student_management/settings.py
+++ student_management/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gf)7+eud9n$v2iyk1#mp%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 
 # Application definition
